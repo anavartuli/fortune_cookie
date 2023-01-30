49f5d6a6018cd4621e7c0c5ba100483e35a4960b
ana carolina <vartuliana@gmail.com>
2023-01-30 14:45:09 -0300

    added script

---
+++ README.md
@@ -0,0 +1,24 @@
+# Fortune Cookie
+
+## What is this?
+
+This is an application that genarates a random fortune cookie message, done as a challenge during the fifth module of the **Rocketseat Explorer** course.
+
+____
+
+## Preview
+<img src="imgs/screen1.png">
+<img src="imgs/screen2.png">
+
+___
+
+## Technologies used in this project
+
+<div style="display:flex">
+
+<img alt="HTML" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg">
+  <img alt="CSS" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg">
+<img alt="JavaScript" height="30" width="40" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg">
+<img alt="Figma" height="30" width="30" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg" />
+
+</div>
---
+++ main.js
@@ -0,0 +1,52 @@
+// variables
+const openTheCookie = document.querySelector("#fortunecookie")
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnReset = document.querySelector("#btnReset")
+
+const fortune = [
+    "A thing is not necessarily true because a man dies for it",
+    "Believe nothing you hear, and only one half that you see",
+    "With freedom, books, flowers, and the moon, who could not be happy?",
+    "I am rooted, but I flow",
+    "Happiness quite unshared can scarcely be called happiness; it has no taste",
+]
+
+let createFortune = document.querySelector('.fortune')
+let selectFortune
+
+
+// events
+openTheCookie.addEventListener('click', handleOpenCookie)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleEnter)
+
+// opening the cookie
+function handleOpenCookie() {
+    toggleScreen()
+
+    let selectFortune = Math.floor(Math.random()*(fortune.length))
+
+    createFortune.innerText = fortune[selectFortune]
+
+}
+
+// get a new fortune
+function handleResetClick(event){
+    event.preventDefault()
+    toggleScreen()
+    
+}
+
+// screen changes
+function toggleScreen(){
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
+
+// open cookie with enter key
+function handleEnter(event){
+    if(event.key == 'Enter' && screen2.classList.contains('hide')){
+        handleResetClick(event)
+    }
+}
